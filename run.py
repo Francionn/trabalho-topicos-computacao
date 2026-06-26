c2761486f92fac9f31e6da39401c887429c5dc53
joaopaulogui <joaopaulogmormota@gmail.com>
2026-06-26 18:51:23 -0300

    feat: add file to run all training scripts and run ui

---
+++ run.py
@@ -0,0 +1,13 @@
+import os
+
+os.system("python3 modelo_etc.py")
+
+os.system("python3 modelo_lr.py")
+
+os.system("python3 modelo_rf.py")
+
+os.system("python3 modelo_svm.py")
+
+os.system("python3 modelo_xgb.py")
+
+os.system("mlflow ui")
